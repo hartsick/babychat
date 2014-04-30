35d23867557d8667d16ed76266317095102b450b
Christa Hartsock <christa.hartsock@gmail.com>
2014-04-29 21:27:43 -0700

    add empty username handling

--- app.js
+++ app.js
@@ -4,9 +4,17 @@ babyApp.controller("BabyCtrl", function($scope, $firebase){
 	var babyRef = new Firebase("https://babychat.firebaseio.com/chat");
 
 	$scope.chat = $firebase(babyRef);
+	$scope.username = "";
 
 	$scope.addMessage = function(){
 		// Add manually using standard JavaScript
+		if ($scope.username == ""){
+			$scope.username = "set yr username, dummy";
+			for (var i = 0; i<10; i++){
+				alert("Hey! Set your name!");
+			}
+		}
+
 		$scope.timestamp = Date.now();
 		babyRef.push( {timestamp:$scope.timestamp, message:$scope.message, username:$scope.username} );
 		// Clear text input after submission
